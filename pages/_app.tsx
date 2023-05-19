6fd743bca3a363734af8146ca7e19b62b97eda17
Alexey Ovsyannikov <soliterco@gmail.com>
2023-05-19 15:21:40 +0300

    added assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import DashboardLayout from "../components/DashboardLayout/DashboardLayout";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <DashboardLayout>
+        <Component {...pageProps} />
+      </DashboardLayout>
+    </ThemeProvider>
+  );
 }
+export default MyApp;
